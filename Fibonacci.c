7c5f09cfc3f8d19e2b1b9b02c5eb94482bae5f97
AlyssonVinicius <alyssonv2003@gmail.com>
2023-03-14 17:41:16 -0300

    Finalizado

--- Fibonacci.c
+++ Fibonacci.c
@@ -1,35 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 int main() {
-    /*inicia por 0 e 1 e o próximo valor sempre será a soma dos 2 valores anteriores
-    (exemplo: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34...)*/
-
-    int prox, penult=0, ult=1, tamFib=0;
-
-    //int n=tamFib, v[n];
-
-    printf("Informe o tamanho do Fibonacci: ");
-        scanf("%d ", &tamFib);
-
-    for (int i = 0; i < tamFib; i++){
-        prox = penult;
-        penult = ult;
-        ult += prox;
-        printf("%d ", ult);
+    int prox, penult=0, ult=1, n;
+    bool seq; // seq = sequencia
+    printf("------------------------------------------------------");
+    printf("\nInforme o para calcular a sequencia do Fibonacci: ");
+        scanf("%d", &n);
+    if(n == 0){
+        seq = true;
+    }else{
+        for (int i = 0; i < n; i++){
+            prox = penult;
+            penult = ult;
+            ult += prox;
+            if(n == ult){
+                seq = true;
+                break;
+            }
+        }
     }
-
-    /*for (int i = 0; i < n; i++){
-        prox = penult;
-        penult = ult;
-        ult += prox;
-        v[i] = ult;
-        printf("%d ", ult);
+    if(seq == true){
+        printf("\nO valor informado SE encontra na sequencia do Fibonacci!\n");
+        printf("------------------------------------------------------");
+    }else{
+        printf("\nO valor informado NAO se encontra na sequencia do Fibonacci!\n");
+        printf("------------------------------------------------------");
     }
-    printf("\n");
-    for (int i = 0; i < n; i++){
-        printf("%i ", v[i]);
-    }*/
-    printf("\nInforme um valor entre 0 e %d: ", ult);
     return 0;
 }
